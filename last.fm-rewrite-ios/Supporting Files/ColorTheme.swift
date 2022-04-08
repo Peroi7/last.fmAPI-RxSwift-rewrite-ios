03c358b4e2f0288169567c7badb6f43037ccb358
Petari7 <ivic.petar77@gmail.com>
2022-04-08 18:25:00 +0200

    Favorite button design changed

--- last.fm-rewrite-ios/Supporting Files/ColorTheme.swift
+++ last.fm-rewrite-ios/Supporting Files/ColorTheme.swift
@@ -13,4 +13,5 @@ struct ColorTheme {
     static let primaryBackground = UIColor(named: "Background-Primary")!
     static let shadowBackground = UIColor(named: "Background-Shadow")!
     static let recordDetailBackground = UIColor(named: "Background-RecordDetail")!
+    static let favoriteButtonBackground = UIColor(named: "Background-Favorite")!
 }
